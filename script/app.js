3a9793cf0fa761a1a9b87c0bb305d41cc7243d7a
Amna khubaib <amnakhubaib123@gmail.com>
2023-04-16 20:20:22 +0500

    Initialize Project

--- script/app.js
+++ script/app.js
@@ -1,7 +1,4 @@
-//total savings
-var totalSavings = prompt("Enter your total savings");
-var zakatAmount = (Number( totalSavings)) * 0.025;
-//zakat Amount
-zakatAmountString = zakatAmount.toFixed(2);
-// final value
-alert("your Zakat Amount is : RS" + zakatAmountString);
+var zakatPercentage = 0.025;
+var userInput = Number(prompt("Enter your total savings"));
+var result = zakatPercentage * userInput;
+alert("Your zakat Amount is " + result);
